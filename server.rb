6aa36738a399e3ce405ca3c16e0c9cfcf1efd488
Stefan Hartmann <stefanhartmann@gmail.com>
2017-06-07 18:38:37 -0400

    add psuedo new views

--- server.rb
+++ server.rb
@@ -7,6 +7,12 @@ Dir["#{current_dir}/models/*.rb"].each { |file| require file }
 module Sinatra
   class Server < Sinatra::Base
     get "/" do
+      @trouts = Trout.all
+      erb :index
+    end
+
+    get "/:id" do
+      @trout = Trout.find(params[:id])
       erb :index
     end
   end
