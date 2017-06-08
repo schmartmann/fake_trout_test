5d9f5fc9d62541a2cc3398514ee3bdd8032dd24e
Stefan Hartmann <stefanhartmann@gmail.com>
2017-06-07 21:35:13 -0400

    remove incorrect dd stuff

--- server.rb
+++ server.rb
@@ -3,24 +3,20 @@ require 'sinatra/activerecord'
 require './config/environment'
 current_dir = Dir.pwd
 Dir["#{current_dir}/models/*.rb"].each { |file| require file }
-require 'datadog/statsd'
 require 'byebug'
 
 module Sinatra
   class Server < Sinatra::Base
     
-    statsd = Datadog::Statsd.new('datadog_test.pet-tinder.com', 5000)
-
     get "/" do
       @trouts = Trout.all
       erb :index
-      statsd.gauge('users.online', 123, :sample_rate=>0.5)
     end
 
     get "/:id" do
       @trout = Trout.find(params[:id])
       erb :index
-      # statsd.increment('page.views')
     end
+
   end
 end
