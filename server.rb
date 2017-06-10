4b43a8da4c0bb130ac9a776e5ee05bb5b4ceeb44
Stefan Hartmann <stefanhartmann@gmail.com>
2017-06-10 17:24:59 -0400

    remove commented code

--- server.rb
+++ server.rb
@@ -1,28 +1,28 @@
-# require 'sinatra/base'
+require 'sinatra/reloader'
 require 'ddtrace'
 require 'ddtrace/contrib/sinatra/tracer'
-# require 'sinatra/activerecord'
 require './config/environment'
 current_dir = Dir.pwd
 Dir["#{current_dir}/models/*.rb"].each { |file| require file }
 
-# module Sinatra
-  # class Server < Sinatra::Base
-  class Sinatra::Application
-    
-    configure do
-      settings.datadog_tracer.configure default_service: "trout_app", debug:true, enabled:true
-    end
-    
-    get "/" do
-      @trouts = Trout.all
-      erb :index
-    end
-
-    get "/:id" do
-      @trout = Trout.find(params[:id])
-      erb :index
-    end
-
+class Sinatra::Application
+  
+  configure do
+    settings.datadog_tracer.configure default_service: "trout_app", debug:true, enabled:true
   end
-# end
+  
+  configure :development do 
+    register Sinatra::Reloader
+  end
+
+  get "/" do
+    @trouts = Trout.all
+    erb :index
+  end
+
+  get "/:id" do
+    @trout = Trout.find(params[:id])
+    erb :index
+  end
+
+end
