b5b97a201226dbb413a49554ba9a4fac2e3213f9
Stefan Hartmann <stefanhartmann@gmail.com>
2017-06-10 16:49:34 -0400

    configure tracer

--- server.rb
+++ server.rb
@@ -10,6 +10,10 @@ Dir["#{current_dir}/models/*.rb"].each { |file| require file }
   # class Server < Sinatra::Base
   class Sinatra::Application
     
+    configure do
+      settings.datadog_tracer.configure default_service: "trout_app", debug:true, enabled:true
+    end
+    
     get "/" do
       @trouts = Trout.all
       erb :index
